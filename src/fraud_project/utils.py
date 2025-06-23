93c3188ef85604eb0c1dadf07762fafb22b4d70c
brunagcsimoes <20240491@novaims.unl.pt>
2025-06-24 00:55:30 +0100

    Update utils.py

--- src/fraud_project/utils.py
+++ src/fraud_project/utils.py
@@ -4,10 +4,17 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-import folium
-from folium.plugins import MarkerCluster
+# import folium
+# from folium.plugins import MarkerCluster
 from difflib import get_close_matches
 import plotly.express as px
 from scipy.stats import chi2_contingency
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
+import logging
+from typing import Tuple, Dict, Any
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from pathlib import Path
+from typing import List, Tuple
