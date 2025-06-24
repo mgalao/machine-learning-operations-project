cb8eff81d732e58f17158c74345647a5feeb7edd
brunagcsimoes <20240491@novaims.unl.pt>
2025-06-24 20:10:14 +0100

    Update scaling.py

--- src/fraud_project/pipelines/preprocessing/utils_preprocessing/scaling.py
+++ src/fraud_project/pipelines/preprocessing/utils_preprocessing/scaling.py
@@ -3,11 +3,12 @@ from fraud_project.utils import *
 # --------------- scaling ---------------------
 def scale_numerical(data: pd.DataFrame, numerical_features: list, scaler: StandardScaler = None) -> Tuple[pd.DataFrame, StandardScaler]:
     data = data.copy()
-    
+
     if scaler is None:
         scaler = StandardScaler()
         data[numerical_features] = scaler.fit_transform(data[numerical_features])
     else:
-        data[numerical_features] = scaler.transform(data[numerical_features])
+        expected_order = list(scaler.feature_names_in_)
+        data[expected_order] = scaler.transform(data[expected_order])
     
     return data, scaler
