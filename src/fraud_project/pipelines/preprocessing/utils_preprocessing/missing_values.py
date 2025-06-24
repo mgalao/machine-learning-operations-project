ff518371bc3dc430d9a23b4b4d06feb76e75dbce
brunagcsimoes <20240491@novaims.unl.pt>
2025-06-24 20:10:17 +0100

    Update missing_values.py

--- src/fraud_project/pipelines/preprocessing/utils_preprocessing/missing_values.py
+++ src/fraud_project/pipelines/preprocessing/utils_preprocessing/missing_values.py
@@ -2,29 +2,44 @@ from fraud_project.utils import *
 
 # --------------- missing values tretament ---------------------
 
-def impute_merch_zipcode(data: pd.DataFrame) -> pd.DataFrame:
+def impute_merch_zipcode(data: pd.DataFrame, mappings: Union[None, dict] = None) -> Union[pd.DataFrame, Tuple[pd.DataFrame, dict]]:
     data = data.copy()
 
-    # impute using most frequent zip per city
-    city_zip_lookup = (
-        data[~data['merch_zipcode'].isna()]
-        .groupby('city')['merch_zipcode']
-        .agg(lambda x: x.mode().iloc[0])
-        .to_dict())
+    if mappings is None:
+        # Training mode — compute mappings
+        city_zip_lookup = (
+            data[~data['merch_zipcode'].isna()]
+            .groupby('city')['merch_zipcode']
+            .agg(lambda x: x.mode().iloc[0])
+            .to_dict()
+        )
+        state_zip_lookup = (
+            data[~data['merch_zipcode'].isna()]
+            .groupby('state')['merch_zipcode']
+            .agg(lambda x: x.mode().iloc[0])
+            .to_dict()
+        )
+    else:
+        # Inference mode — use existing mappings
+        city_zip_lookup = mappings.get("city_zip_lookup", {})
+        state_zip_lookup = mappings.get("state_zip_lookup", {})
+
+    # Impute using city mapping
     mask_city = data['merch_zipcode'].isna() & data['city'].notna()
     data.loc[mask_city, 'merch_zipcode'] = data.loc[mask_city, 'city'].map(city_zip_lookup)
 
-    # impute remaining using most frequent zip per state
-    state_zip_lookup = (
-        data[~data['merch_zipcode'].isna()]
-        .groupby('state')['merch_zipcode']
-        .agg(lambda x: x.mode().iloc[0])
-        .to_dict()
-    )
+    # Impute using state mapping
     mask_state = data['merch_zipcode'].isna() & data['state'].notna()
     data.loc[mask_state, 'merch_zipcode'] = data.loc[mask_state, 'state'].map(state_zip_lookup)
 
-    # final flag for rows still missing after imputations
+    # Final missing flag
     data['zip_missing'] = data['merch_zipcode'].isna().astype(int)
 
-    return data
+    if mappings is None:
+        # Return mappings only in training mode
+        return data, {
+            "city_zip_lookup": city_zip_lookup,
+            "state_zip_lookup": state_zip_lookup
+        }
+    else:
+        return data
