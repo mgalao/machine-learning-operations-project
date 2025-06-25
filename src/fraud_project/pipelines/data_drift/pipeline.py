a8588644c792d3f4723f8fc17bdb80f551ff027b
Daniel Caridade <20211588@novaims.unl.pt>
2025-06-25 22:24:42 +0100

    Update pipeline.py

--- src/fraud_project/pipelines/data_drift/pipeline.py
+++ src/fraud_project/pipelines/data_drift/pipeline.py
@@ -4,19 +4,19 @@
 # generated using Kedro 0.18.8
 # """
 
-# from kedro.pipeline import Pipeline, node, pipeline
+from kedro.pipeline import Pipeline, node, pipeline
 
-# from .nodes import data_drift
+from .nodes import data_drift
 
-# def create_pipeline(**kwargs) -> Pipeline:
-#     return pipeline(
-#         [
-#             node(
-#                 func= data_drift,
-#                 inputs=["ref_data","ana_data"],
-#                 outputs= "drift_result",
-#                 name="data_drift",
-#             ),
+def create_pipeline(**kwargs) -> Pipeline:
+    return pipeline(
+         [
+             node(
+                 func= data_drift,
+                 inputs=["ref_data","ana_data"],
+                 outputs= "drift_result",
+                 name="data_drift",
+             ),
 
-#         ]
-#     )
+         ]
+     )
