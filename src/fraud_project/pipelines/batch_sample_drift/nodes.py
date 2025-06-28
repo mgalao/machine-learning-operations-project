72e0e771b83e8cbfbdf157618385160a5d125708
Daniel Caridade <20211588@novaims.unl.pt>
2025-06-28 01:17:36 +0100

    Update nodes.py

--- src/fraud_project/pipelines/batch_sample_drift/nodes.py
+++ src/fraud_project/pipelines/batch_sample_drift/nodes.py
@@ -1,11 +1,11 @@
-from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score
 import pandas as pd
+from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score
 
-# Function to create a sample data frame
+
 def filter_test_data_by_state(ana_data: pd.DataFrame, state: str = 'NY') -> pd.DataFrame:
     return ana_data[ana_data["state"] == state].copy()
 
-# Evaluate the evaluation metrics on drift data
+
 def evaluate_predictions(df_with_predict: pd.DataFrame, target_column: str = "is_fraud") -> pd.DataFrame:
     y_true = df_with_predict[target_column]
     y_pred = df_with_predict["prediction"]
@@ -17,4 +17,9 @@ def evaluate_predictions(df_with_predict: pd.DataFrame, target_column: str = "is
         "macro_recall": recall_score(y_true, y_pred, average="macro"),
     }
 
-    return pd.DataFrame([metrics])
+    result_df = pd.DataFrame([metrics])
+
+    # Save to reporting folder
+    result_df.to_csv("data/08_reporting/eval_metric_drift_data.csv", index=False)
+
+    return result_df
