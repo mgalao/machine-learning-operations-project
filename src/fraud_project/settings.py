1142a8c4ed912cbf63234e3fbce2692fb11ed4fe
Daniel Caridade <20211588@novaims.unl.pt>
2025-06-28 15:16:56 +0100

    Update settings.py

--- src/fraud_project/settings.py
+++ src/fraud_project/settings.py
@@ -4,9 +4,8 @@ https://kedro.readthedocs.io/en/stable/kedro_project_setup/settings.html."""
 
 # Instantiated project hooks.
 # For example, after creating a hooks.py and defining a ProjectHooks class there, do
-# from fraud_project.hooks import ProjectHooks
-# Hooks are executed in a Last-In-First-Out (LIFO) order.
-# HOOKS = (ProjectHooks(),)
+from fraud_project.hooks.custom_hooks import DriftHook
+HOOKS = (DriftHook(),)
 
 # Installed plugins for which to disable hook auto-registration.
 # DISABLE_HOOKS_FOR_PLUGINS = ("kedro-viz",)
