16995f3fdd6c7843be5b2b4d74705fd25a291cfa
Daniel Caridade <20211588@novaims.unl.pt>
2025-06-28 23:43:42 +0100

    Update pipeline.py

--- src/fraud_project/pipelines/model_train/pipeline.py
+++ src/fraud_project/pipelines/model_train/pipeline.py
@@ -16,8 +16,6 @@ def create_pipeline(**kwargs) -> Pipeline:
                     "X_test_data",
                     "y_train_data",
                     "y_test_data",
-                    "model_cls",
-                    "model_params",
                     "parameters",
                     "best_columns"
                 ],
